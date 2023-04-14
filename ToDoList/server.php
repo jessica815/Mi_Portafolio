a222a92ec6922b4143d97387e7c364d27a717b2a
Jessica Medina Carrillo <71737669+jessica815@users.noreply.github.com>
2023-04-14 17:20:48 -0600

    ToDoList y PWA
    Esta es una ToDoList creada con diversas herramientas de desarrollo para la Universidad de Colima y a la vez es una PWA que se puede descargar en tú móvil y funciona offline.

---
+++ ToDoList/server.php
@@ -0,0 +1,12 @@
+<?php
+
+    $data = file_get_contents("php://input");
+
+    $datos = json_decode($data, true);
+
+    $archivo = $datos[0];
+    $tareas = $datos[1];
+
+    $bytes = file_put_contents("datos/$archivo", json_encode($tareas));
+    echo $bytes;
+?>
