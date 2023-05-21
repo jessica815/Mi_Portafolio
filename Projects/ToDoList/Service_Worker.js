75d1d5b5d2751b33da2442afe83ff790c7e02416
Jessica Medina Carrillo <71737669+jessicamedinacarrillo@users.noreply.github.com>
2023-05-20 21:28:53 -0600

    Add files via upload

---
+++ Projects/ToDoList/Service_Worker.js
@@ -0,0 +1,131 @@
+// This is the "Offline copy of assets" service worker
+//cacheTo
+
+const CACHE_NAME = 'ToDoList' , urlToCache = [
+    /*Aquí se cargan todos los archivos que utilizaré para mi PWA*/
+    './',
+    './index.php',
+    './css/style.css',
+    './css/bootstrap.min.css',
+    './css/font-awesome.min.css',
+  
+    './registro_serviceWorker.js',
+    './Service_Worker.js',
+  
+    './images/icono.png',
+    './images/icon-24x24.png',
+    './images/icon-42x42.png',
+    './images/icon-72x72.png',
+    './images/icon-96x96.png',
+    './images/icon-128x128.png',
+    './images/icon-144x144.png',
+    './images/icon-152x152.png',
+    './images/icon-192x192.png',
+    './images/icon-256x256.png',
+    './images/icon-384x384.png',
+    './images/icon-512x512.png',
+    './main.js',
+    './manifest.json',
+
+    './fonts/fontawesome-webfont.eot',
+    './fonts/fontawesome-webfont.svg',
+    './fonts/fontawesome-webfont.ttf',
+    './fonts/fontawesome-webfont.woff',
+    './fonts/fontawesome-webfont.woff2',
+    './fonts/FontAwesome.otf',
+    './fonts/Roboto-Regular.eot',
+    './fonts/Roboto-Regular.otf',
+    './fonts/Roboto-Regular.svg',
+    './fonts/Roboto-Regular.ttf',
+    './fonts/Roboto-Regular.woff',  
+  ];
+  
+//Durante la fase de instalación, generalmente se almacena en caché los activos estáticos.
+/*
+self.addEventListener('install', e => {
+    e.waitUntil(
+      caches.open(CACHE_NAME)
+        .then(cache => {
+          return cache.addAll(u )
+            .then(() => self.skipWaiting())
+        })
+        .catch(err => console.log('Falló registro de cache', err))
+    )
+})
+*/
+
+//add listener to install event
+self.addEventListener('install', function (event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(function (cache) {
+                console.log('Opened cache');
+                return cache.addAll(urlToCache);
+            })  
+    );
+});
+
+
+
+  
+//Una vez que se instala el SW, se activa y busca los recursos para hacer que funcione sin conexión.
+self.addEventListener('activate', e => {
+    const cacheWhitelist = [CACHE_NAME]
+  
+    e.waitUntil(
+      caches.keys()
+        .then(cacheNames => {
+          return Promise.all(
+            cacheNames.map(cacheName => {
+              //Eliminamos lo que ya no se necesita en cache
+              if (cacheWhitelist.indexOf(cacheName) === -1) {
+                return caches.delete(cacheName)
+              }
+            })
+          )
+        })
+        // Le indica al SW activar el cache actual
+        .then(() => self.clients.claim())
+    )
+})
+  
+//Cuando el navegador recupera una url
+self.addEventListener('fetch', e => {
+    //Responder ya sea con el objeto en caché o continuar y buscar la url real
+    e.respondWith(
+      caches.match(e.request)
+        .then(res => {
+          if (res) {
+            //Recuperar del cache
+            return res
+          }
+          //Recuperar de la petición a la url
+          return fetch(e.request)
+        })
+    )
+})
+
+// Notificacion push with firebase
+// self.addEventListener('push', function(event) {
+//   console.log('Push recieved', event);
+//   var title = 'Ya casi estamos listos';
+//   var body = 'Para poder usar nuestra aplicación, debes registrarte';
+//   var icon = './images/icon-512x512.png';
+//   event.waitUntil(
+//     self.registration.showNotification(title, {
+//       body: body,
+//       icon: icon
+//     })
+//   );
+// });
+
+
+// self.addEventListener('push', (event) => {
+//   const json = JSON.parse(event.data.text()
+
+//   );
+//   console.log('Push Data', event.data.text())
+//   self.registration.showNotification(json.header, json.options)
+// });
+
+
